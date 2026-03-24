4d3983231eac42900864fc004a44c37c3bced5b0
maveryn <xashru@gmail.com>
2026-03-24 01:13:55 +0000

    Deploying to gh-pages from @ maveryn/maveryn.github.io@d213644b07803017fc12336593ecc4fd7a381a6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Selected publications and abstracts.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
